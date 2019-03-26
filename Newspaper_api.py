d43af2bb61015b82eea795b6257b63be997560ae
Tim Grob <tim.grob@swissonline.ch>
2019-03-26 15:15:41 +0100

    Upload scraper file

---
+++ Newspaper_api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+app_id = 'ebd056b2-12ba-4233-9673-b35102f9a4dc'
+api_key = 'yLQx1LlB0u5YAqjus1vYhBDa6ILAXObk'
+keyword = 'election'
+
+url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={}&api-key={}'.format(keyword, api_key)
+response = requests.get(url)
+data = response.json()
+print(len(data["response"]["docs"]))
+
+for i in data['response']['docs']:
+    print(i['web_url'])
