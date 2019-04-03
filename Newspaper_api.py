b325d9e34fb960fb1d31e61ebd8a3e14c96cab1a
Lisa Leyla Yerebakan <lisa.yerebakan@student.unisg.ch>
2019-04-03 09:40:13 +0200

    found this helper function for time iteration on github, still needs adjusting though?
    also need to work on loop :/

--- Newspaper_api.py
+++ Newspaper_api.py
@@ -1,9 +1,19 @@
 import requests
-
+import datetime
+import time
 
 app_id = 'ebd056b2-12ba-4233-9673-b35102f9a4dc'
 api_key = 'yLQx1LlB0u5YAqjus1vYhBDa6ILAXObk'
-keyword = 'election'
+keyword = 'libya'
+
+# helper function to iterate through dates
+def daterange( start_date, end_date ):
+    if start_date <= end_date:
+        for n in range( ( end_date - start_date ).days + 1 ):
+            yield start_date + datetime.timedelta( n )
+    else:
+        for n in range( ( start_date - end_date ).days + 1 ):
+            yield start_date - datetime.timedelta( n )
 
 url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={}&api-key={}'.format(keyword, api_key)
 response = requests.get(url)
@@ -12,4 +22,3 @@ print(len(data["response"]["docs"]))
 
 for i in data['response']['docs']:
     print(i['web_url'])
-#Danke! Lisa
